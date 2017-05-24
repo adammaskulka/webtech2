7ebca1a9b76232d2083bec3482ee5b86f50a1498
xskopek <skopek.radoslav@gmail.com>
2017-05-24 22:51:15 +0200

    upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,9 @@
 <?php
    define ('SITE_ROOT', realpath(dirname(__FILE__)));
-   $uploadfile = SITE_ROOT . '/uploads/' . basename($_FILES['userfile']['name']);
+   $uploadfile = SITE_ROOT . '/uploads/' . basename($_FILES['subor']['name']);
    $document_id = $_POST['Document_id'];
    $link = $_POST['nameF'];
-   if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+   if (move_uploaded_file($_FILES['subor']['tmp_name'], $uploadfile)) {
      echo "File is valid, and was successfully uploaded.\n";
      require('cfg/config.php');
      $conn = new mysqli($CONF_DB_HOST, $CONF_DB_USER, $CONF_DB_PASS, $CONF_DB_NAME);
@@ -14,7 +14,7 @@
      $sql = "INSERT INTO attachments (name,link, fk_documents) VALUES (?, ?, ?)";
 
      $stmt = $conn->prepare($sql);
-     $stmt->bind_param("ssi", basename($_FILES['userfile']['name']),$link, $document_id);//i - integer d - double s - string b - BLOB
+     $stmt->bind_param("ssi", basename($_FILES['subor']['name']),$link, $document_id);//i - integer d - double s - string b - BLOB
 
      $stmt->execute();
    } else {
