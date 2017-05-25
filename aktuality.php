4f744123b96888b4c0750671f8426dd7b9b0399a
Millenium47 <andrejd47@gmail.com>
2017-05-25 23:42:56 +0300

    Update aktuality.php

--- aktuality.php
+++ aktuality.php
@@ -121,7 +121,7 @@ $mysqli->close();
 <!--breadcrumbs end-->
 
 <?php
-require('config.php');
+require('cfg/config.php');
 $mysqli = new mysqli($CONF_DB_HOST, $CONF_DB_USER, $CONF_DB_PASS, $CONF_DB_NAME);
 if($mysqli->connect_error){
 die("Connect error" . $mysqli->connect_error);
