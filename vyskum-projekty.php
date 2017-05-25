c415b07f571a01789b0f6473c129c18d416fb42f
Millenium47 <andrejd47@gmail.com>
2017-05-25 18:33:29 +0300

    Update vyskum-projekty.php

--- vyskum-projekty.php
+++ vyskum-projekty.php
@@ -106,7 +106,7 @@ session_start();
 		<!--container start--tu zacianuju tabulky-->
         <div class="row">
 		<?php
-		require('config.php');
+		require('cfg/config.php');
 		$mysqli = new mysqli($CONF_DB_HOST, $CONF_DB_USER, $CONF_DB_PASS, $CONF_DB_NAME);
 		if($mysqli->connect_error){
 		die("Connect error" . $mysqli->connect_error);
