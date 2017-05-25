3bd04bc8a76e7f0a683723d94e503345ce45f153
xskopek <skopek.radoslav@gmail.com>
2017-05-25 18:47:44 +0200

    multilanguage everywhere
    multilanguage, multilanguage everywhere

--- vyskum-biomechanika.php
+++ vyskum-biomechanika.php
@@ -1,3 +1,14 @@
+<?php 
+	session_start();
+	
+	if(isset($_GET['lang'])){
+		$_SESSION['lang'] = $_GET['lang'];
+	}
+	
+	if(!isset($_SESSION['lang']))  
+		$_SESSION['lang'] = 'sk';
+
+?>
 <!DOCTYPE html>
 <html lang="sk">
 <head>
@@ -54,222 +65,13 @@
 
 <body>
 <!--header start-->
-<header class="head-section">
-    <div class="navbar navbar-default navbar-static-top container">
-        <div class="navbar-header">
-            <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse"
-                    type="button"><span class="icon-bar"></span> <span class="icon-bar"></span>
-                <span class="icon-bar"></span></button>
-            <a class="navbar-brand" href="index.php">FEI M<span>echatronika</span></a>
-        </div>
-
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-close-others="false" data-delay="0" data-hover=
-                    "dropdown" data-toggle="dropdown" href="#">O nás<i class="fa fa-angle-down"></i>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="history.php">História</a>
-                        </li>
-                        <li>
-                            <a href="management.php">Vedenie ústavu</a>
-                        </li>
-
-                        <li class="dropdown-submenu">
-                            <a href="#" tabindex="-1">Oddelenia</a>
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-submenu"></li>
-
-                                <li>
-                                    <a href="department_oamm.php">Oddelenie aplikovanej mechaniky a mechatroniky
-                                        (OAMM)</a>
-                                </li>
-
-                                <li>
-                                    <a href="department_oikr.php">Oddelenie informačných, komunikačných a riadiacich
-                                        systémov (OIKR)</a>
-                                </li>
-                                <li>
-                                    <a href="department_oemp.php">Oddelenie elektroniky, mikropočítačov a PLC systémov
-                                        (OEMP)</a>
-                                </li>
-
-                                <li>
-                                    <a href="department_oeap.php">Oddelenie E-mobility, automatizácie a pohonov
-                                        (OEAP)</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="pracovnici.php">Pracovníci</a>
-                </li>
-
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-close-others="false" data-delay="0" data-hover=
-                    "dropdown" data-toggle="dropdown" href="#">Štúdium<i class="fa fa-angle-down"></i>
-                    </a>
-
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-submenu">
-                            <a href="#" tabindex="-1">Pre uchádzačov o štúdium</a>
-
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-submenu"></li>
-
-                                <li>
-                                    <a href="applicants-bachelor.php">Bakalárske štúdium</a>
-                                </li>
-
-                                <li>
-                                    <a href="applicants-master.php">Inžinierske štúdium</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown-submenu">
-                            <a href="#" tabindex="-1">Bakalárske štúdium</a>
-
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-submenu"></li>
-
-                                <li>
-                                    <a href="bachelor-info.php">Všeobecné informácie</a>
-                                </li>
-
-                                <li class="dropdown-submenu">
-                                    <a href="#">Bakalárske práce</a>
-
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a href="bachelor-instructions.php">Pokyny</a>
-                                        </li>
-
-                                        <li>
-                                            <a href="bachelor-thesis.php">Voľné témy</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li class="dropdown-submenu">
-                            <a href="#" tabindex="-1">Inžinierske štúdium</a>
-
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-submenu"></li>
-
-                                <li>
-                                    <a href="master-info.php">Všeobecné informácie</a>
-                                </li>
-
-                                <li class="dropdown-submenu">
-                                    <a href="#">Diplomové práce</a>
-
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a href="master-instructions.php">Pokyny</a>
-                                        </li>
-
-                                        <li>
-                                            <a href="master-thesis.php">Voľné témy</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a href="doctor-info.php">Doktorandské štúdium</a>
-                        </li>
-                    </ul>
-                </li>
-
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-close-others="false" data-delay="0" data-hover=
-                    "dropdown" data-toggle="dropdown" href="#">Výskum<i class="fa fa-angle-down"></i>
-                    </a>
-
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="vyskum-projekty.php">Projekty</a>
-                        </li>
-
-                        <li class="dropdown-submenu">
-                            <a href="#" tabindex="-1">Výskumné oblasti</a>
-
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-submenu"></li>
-
-                                <li>
-                                    <a href="vyskum-motokara.php">Elektrická motokára</a>
-                                </li>
-
-                                <li>
-                                    <a href="vyskum-vozidlo.php">Autonómne vozidlo 6×6</a>
-                                </li>
-                                <li>
-                                    <a href="vyskum-kocka.php">3D LED kocka</a>
-                                </li>
-
-                                <li>
-                                    <a href="vyskum-biomechanika.php">Biomechatronika</a>
-                                </li>
-                            </ul>
-                        </li>
-
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="aktuality.php">Aktuality</a>
-                </li>
-
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-close-others="false" data-delay="0" data-hover=
-                    "dropdown" data-toggle="dropdown" href="#">Aktivity<i class="fa fa-angle-down"></i>
-                    </a>
-
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="photo-galery.php">Fotogaléria</a>
-                        </li>
-                        <li>
-                            <a href="video-galery.php">Videá</a>
-                        </li>
-                        <li>
-                            <a href="media.php">Média</a>
-                        </li>
-
-                        <li class="dropdown-submenu">
-                            <a href="#" tabindex="-1">Naše tématické web stránky</a>
-
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-submenu"></li>
-
-                                <li>
-                                    <a href="http://www.e-mobilita.fei.stuba.sk">Elektromobilita</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="contact.php">Kontakt</a>
-                </li>
-
-                <li>
-                    <a href="login.php">Intranet</a>
-                </li>
-
-            </ul>
-        </div>
-    </div>
-</header>
+<?php
+	if(strcmp($_SESSION['lang'],'en') == 0)
+		include('header-en.php'); 
+	if(strcmp($_SESSION['lang'],'sk') == 0)
+		include('header-sk.php'); 
+?>
+ 
 <!--header end-->
 
 <!--breadcrumbs start-->
@@ -311,105 +113,12 @@
 <!--container end-->
 
 <!--footer start-->
-<footer class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-sm-3">
-                <div class="text-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".1s">
-                    <h1>
-                        O nás
-                    </h1>
-                    <p>
-                        Ústav automobilovej mechatroniky bol zriadený k 1. júlu 2013 ako pedagogické a vedecko-výskumné
-                        pracovisko FEI STU. Zriadenie ústavu bolo logickým vyústením zámerov vedenia FEI STU vytvoriť
-                        taký ústav, ktorý by zohľadňoval súčasné požiadavky a potreby automobilového priemyslu.
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-sm-3 address wow fadeInUp" data-wow-duration="2s" data-wow-delay=".2s">
-                <h1>
-                    Kontakt
-                </h1>
-                <address>
-                    <p><i class="fa fa-home pr-10"></i>Fakulta elektrotechniky a informatiky</p>
-                    <p><i class="fa fa-globe pr-10"></i>Ilkovičova 3, 812 19 Bratislava</p>
-                    <p><i class="fa fa-mobile pr-10"></i>+421 260 291 598</p>
-                    <p><i class="fa fa-envelope pr-10"></i><a href="mailto:uamt.fei@gmail.com">uamt.fei@gmail.com</a>
-                    </p>
-                </address>
-            </div>
-
-            <div class="col-lg-3 col-sm-3">
-                <div class="page-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">
-                    <h1>
-                        Užitočné stránky
-                    </h1>
-                    <ul class="page-footer-list">
-                        <li>
-                            <i class="fa fa-angle-right"></i>
-                            <a href="http://is.stuba.sk">AIS STU</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right"></i>
-                            <a href="http://aladin.elf.stuba.sk/rozvrh">Rozvrh hodín FEI</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right"></i>
-                            <a href="http://elearn.elf.stuba.sk/moodle">Moodle FEI</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right"></i>
-                            <a href="http://www.sski.sk/webstranka">SSKI</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right"></i>
-                            <a href="https://www.jedalen.stuba.sk/WebKredit">Jedáleň STU</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right"></i>
-                            <a href="https://webmail.stuba.sk">Webmail STU</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right"></i>
-                            <a href="https://kis.cvt.stuba.sk/i3/epcareports/epcarep.csp?ictx=stu&language=1">Evidencia
-                                publikácií STU</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right"></i>
-                            <a href="http://okocasopis.sk">Časopis OKO</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-3 col-sm-3 wow fadeInUp" data-wow-duration="2s" data-wow-delay=".4s">
-                <h1>POZRI SI VIDEO :)</h1>
-                <iframe src="//www.youtube.com/embed/XTjNQoEZmbk" frameborder="0" allowfullscreen=""></iframe>
-            </div>
-        </div>
-    </div>
-</footer>
-<!-- footer end -->
-<!--small footer start -->
-<footer class="footer-small">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-sm-6 pull-right">
-                <ul class="social-link-footer list-unstyled">
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".1s"><a
-                                href="https://www.facebook.com/UAMTFEISTU"><i class="fa fa-facebook"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".2s"><a
-                                href="https://www.youtube.com/channel/UCo3WP2kC0AVpQMIiJR79TdA"><i
-                                    class="fa fa-youtube"></i></a></li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <div class="copyright">
-                    <p>&copy; Copyright - 2017</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
+<?php 
+	if(strcmp($_SESSION['lang'],'en') == 0)
+		include('footer-en.php'); 
+	if(strcmp($_SESSION['lang'],'sk') == 0)
+		include('footer-sk.php'); 
+?>
 <!--small footer end-->
 
 <!-- js placed at the end of the document so the pages load faster
