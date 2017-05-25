985f85d45470729ab13073df66fd018a71df8f4b
Millenium47 <andrejd47@gmail.com>
2017-05-25 22:17:36 +0300

    Update vyskum-kocka.php

--- vyskum-kocka.php
+++ vyskum-kocka.php
@@ -18,7 +18,7 @@
     <meta name="description" content="Ústav automobilovej mechatroniky FEI STU">
 
     <title>
-        Ústav automobilovej mechatroniky FEI STU | Výskum - 3D LED Kocka
+	<?php if($_SESSION['lang'] == 'sk') echo "Ústav automobilovej mechatroniky FEI STU | Výskum - 3D LED Kocka"; else echo "Institute of Automotive Mechatronics FEI STU Research - 3D LED Cube"; ?>
     </title>
 
     <!-- Bootstrap core CSS -->
@@ -79,12 +79,12 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-sm-4">
-                <h1>Výskum - 3D LED Kocka</h1>
+                <h1><?php if($_SESSION['lang'] == 'sk') echo "Výskum - 3D LED Kocka"; else echo "Research - 3D LED Cube"; ?></h1>
             </div>
             <div class="col-lg-8 col-sm-8">
                 <ol class="breadcrumb pull-right">
-                    <li><a href="index.php">Domov</a></li>
-                    <li class="active">Výskum - 3D LED Kocka</li>
+                    <li><a href="index.php"><?php if($_SESSION['lang'] == 'sk') echo "Domov"; else echo "Home"; ?></a></li>
+                    <li class="active"><?php if($_SESSION['lang'] == 'sk') echo "Výskum - 3D LED Kocka"; else echo "Research - 3D LED Cube"; ?></li>
                 </ol>
             </div>
         </div>
@@ -106,7 +106,8 @@
         <div class="row">
             <img src="kocka.jpg" alt="Kocka" style="width: 100%; height:auto;">
 		    <p>
-		    Zobrazená kocka bola vytvorená v rámci diplomovej práce. Bol k nej vytvorený vzdialený prístup cez Internet, čo umožňuje užívateľovi vkladať do kocky vlastný kód, ktorý ovplyvňuje jej správanie sa.
+		<?php if($_SESSION['lang'] == 'sk') echo "Zobrazená kocka bola vytvorená v rámci diplomovej práce. Bol k nej vytvorený vzdialený prístup cez Internet, čo umožňuje užívateľovi vkladať do kocky vlastný kód, ktorý ovplyvňuje jej správanie sa."; else echo "Displayed cube was created within the diploma thesis. It was created for remote access via the Internet. It allows the user to insert custom code blocks and in this way to influence its behavior."; ?>
+		    
 		    </p>
 
         </div>
